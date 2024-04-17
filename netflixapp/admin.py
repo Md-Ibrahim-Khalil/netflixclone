146a727b73307f3251a99a0bf2673e854cc07aa3
Md-Ibrahim-Khalil <ibrahimcse.eng@gmail.com>
2024-04-17 13:16:49 +0600

    Database and Model Setup

--- netflixapp/admin.py
+++ netflixapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Video, Profile, CustomUser
+
+admin.site.register(Movie)
+admin.site.register(Video)
+admin.site.register(Profile)
+admin.site.register(CustomUser)
